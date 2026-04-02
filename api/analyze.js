db46d96d71a8958dad33ffb91bdd314947fb44c7
pjx687bnps-a11y <seainvest@yahoo.com>
2026-04-02 12:12:55 +0300

    Update analyze.js

--- api/analyze.js
+++ api/analyze.js
@@ -1,9 +1,3 @@
-import { createClient } from "@supabase/supabase-js";
-
-const supabase = createClient(
-  process.env.SUPABASE_URL,
-  process.env.SUPABASE_SERVICE_ROLE_KEY
-);
 export default async function handler(req, res) {
   try {
     const body =
